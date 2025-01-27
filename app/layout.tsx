e24b5510184cb8de9fcdc154207e348c6cd8490d
anirudtate <anirud.programming@gmail.com>
2025-01-27 17:18:49 +0530

    setup emoji font for users who don't have one on there system

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,18 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Noto_Color_Emoji } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/lib/auth-context";
+import { cn } from "@/lib/utils";
 
 const fontSans = Inter({
   variable: "--font-sans",
   subsets: ["latin"],
+});
+
+const fontEmoji = Noto_Color_Emoji({
+  variable: "--font-emoji",
+  subsets: ["emoji"],
+  weight: "400",
 });
 
 export const metadata: Metadata = {
@@ -27,7 +34,13 @@ export default function RootLayout({
           href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>✨</text></svg>"
         />
       </head>
-      <body className={`${fontSans.variable} antialiased`}>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable,
+          fontEmoji.variable
+        )}
+      >
         <AuthProvider>{children}</AuthProvider>
       </body>
     </html>
